1b561d1fd825886f250ff143e4f1217c5946a505
basim2003g <dhdjdhbx@gmail.com>
2026-05-23 13:28:30 +0300

    Add update and delete features to tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/about', function() {
     $name = 'Basim';
@@ -22,4 +23,35 @@ Route::post('/about', function() {
     ];
 
     return view('about', compact('name', 'departments'));
+});
+
+Route::get('tasks', function() {
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks', compact('tasks'));
+});
+
+Route::post('create', function() {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+
+    return redirect()->back();
+});
+Route::post('delete/{id}', function($id) {
+    DB::table('tasks')->where('id', $id)->delete();
+
+    return redirect()->back();
+});
+Route::post('edit/{id}', function($id) {
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks', compact('task', 'tasks'));
+});
+
+Route::post('update', function() {
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id', '=', $id)->update(['name' => $_POST['name']]);
+
+    return redirect('tasks');
 });
